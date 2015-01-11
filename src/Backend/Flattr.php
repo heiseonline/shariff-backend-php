986941a2712b7b457b2057b2523b90eb180e901a
Ludwig Behm <ludwig.behm@gmail.com>
2015-01-11 11:02:14 +0100

    New service: Flattr

---
+++ src/Backend/Flattr.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Heise\Shariff\Backend;
+
+class Flattr extends Request implements ServiceInterface
+{
+
+    public function getName()
+    {
+        return 'flattr';
+    }
+
+    public function getRequest($url)
+    {
+        return $this->createRequest('https://api.flattr.com/rest/v2/things/lookup/?url='.urlencode($url));
+	}
+
+    public function extractCount($data)
+    {
+        return ($data['flattrs']) ? $data['flattrs'] : 0;
+    }
+}
