0f1f7f6f90357df6a22c21abec34b77585a2c529
Ludwig Behm <ludwig.behm@gmail.com>
2015-01-13 02:16:59 +0100

    Fix: Facebook Backend
    Fix for major codebreaker!
    Found by running tests - we should do this more often!

--- build/src/Backend/Facebook.php
+++ build/src/Backend/Facebook.php
@@ -12,12 +12,13 @@ class Facebook extends Request implements ServiceInterface
 
     public function getRequest($url)
     {
-        return $this->createRequest('https://graph.facebook.com/?id='.$url);
+        $fql = 'https://graph.facebook.com/fql?q=SELECT total_count FROM link_stat WHERE url="'.$url.'"';
+        return $this->createRequest($fql);
     }
 
     public function extractCount($data)
     {
-        return (isset($data['shares'])) ? $data['shares'] : 0;
+        return (isset($data['data']) && isset($data['data'][0]) && isset($data['data'][0]['total_count']))
+            ? $data['data'][0]['total_count'] : 0;
     }
 }
-
