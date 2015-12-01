33595bef44207b54a1141e26f92d9a9177416c95
craiq <craiq@live.de>
2015-12-01 21:32:26 +0100

    clean build folder

--- build.sh
+++ build.sh
@@ -3,6 +3,12 @@
 mkdir -p build
 rm -fr build/*
 composer install --no-dev
+if [ -d "cleaner" ]; then
+  php cleaner/bin/composer-cleaner
+else
+  composer create-project dg/composer-cleaner cleaner v0.2
+  php cleaner/bin/composer-cleaner
+fi
 cp index.php build
 cp shariff.json build
 cp -a vendor build
