0bf595144ed17ec7828e028c1932c10af26d3ff9
apl <apl@heise.de>
2016-04-01 09:34:58 +0200

    run php-cs-fixer

--- src/Backend/Request.php
+++ src/Backend/Request.php
@@ -30,11 +30,13 @@ abstract class Request
      * @param array  $options This parameter is ignored and is only present for backwards compatibility reasons.
      *
      * @return RequestInterface
+     *
      * @deprecated This method is not used anymore and will be removed with version 6. Use \GuzzleHttp\Psr7\Request directly instead.
      */
     protected function createRequest($url, $method = 'GET', $options = [])
     {
         trigger_error('This method is not used anymore and will be removed with version 6. Use \GuzzleHttp\Psr7\Request directly instead.', E_USER_DEPRECATED);
+
         return new \GuzzleHttp\Psr7\Request($method, $url);
     }
 
