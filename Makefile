992a4bc2f2382efb76a62f5847a50fbe4c422046
Richard Fath <richard.fath@t-online.de>
2023-10-28 12:00:45 +0200

    Change back -vvv to --verbose for php-cs-fixer

--- Makefile
+++ Makefile
@@ -1,5 +1,5 @@
 cs:
-	vendor/bin/php-cs-fixer fix -vvv
+	vendor/bin/php-cs-fixer fix --verbose
 
 cs_dry_run:
-	vendor/bin/php-cs-fixer fix -vvv --dry-run --diff
+	vendor/bin/php-cs-fixer fix --verbose --dry-run --diff
--- test.sh
+++ test.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 vendor/bin/phpcs --standard=PSR12 src/ tests/
-vendor/bin/php-cs-fixer fix -vvv --dry-run --diff
+vendor/bin/php-cs-fixer fix --verbose --dry-run --diff
 
 export XDEBUG_MODE=coverage
 vendor/bin/phpunit --coverage-text -v
