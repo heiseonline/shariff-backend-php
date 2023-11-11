e331b6d9e183286869a9349ae25807119ed3716f
Richard Fath <richard.fath@t-online.de>
2023-11-11 13:51:33 +0100

    Run PHPCS also for index.php

--- test.sh
+++ test.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-vendor/bin/phpcs --standard=PSR12 src/ tests/
+vendor/bin/phpcs --standard=PSR12 index.php src/ tests/
 vendor/bin/php-cs-fixer fix --verbose --dry-run --diff
 
 export XDEBUG_MODE=coverage
